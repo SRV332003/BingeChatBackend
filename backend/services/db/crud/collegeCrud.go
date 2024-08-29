9045865bc64ca87bd6acb28a4094b24a6b768bbc
Sourav <srvgarg332003@gmail.com>
2024-08-29 16:41:15 +0530

    Reduce the output data of get colleges

--- backend/services/db/crud/collegeCrud.go
+++ backend/services/db/crud/collegeCrud.go
@@ -5,6 +5,11 @@ import (
 	"HangAroundBackend/services/db"
 )
 
+type College struct {
+	Name string `json:"name"`
+	ID   uint   `json:"id"`
+}
+
 func CreateCollege(college *models.College) error {
 	result := db.Instance.Create(college)
 	if result.Error != nil {
@@ -22,12 +27,14 @@ func GetCollegeById(id uint) (*models.College, error) {
 	return &college, nil
 }
 
-func GetAllColleges() ([]models.College, error) {
-	var colleges []models.College
-	result := db.Instance.Find(&colleges, "verified = ?", true)
+func GetAllCollegesNames() ([]College, error) {
+	var colleges []College
+	// select only name and id
+	result := db.Instance.Select("name", "id").Find(&colleges)
 	if result.Error != nil {
 		return nil, result.Error
 	}
+
 	return colleges, nil
 }
 
