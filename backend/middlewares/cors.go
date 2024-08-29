d3586e70671f9c92f5438ccc5405a2a5dd3f4299
Sourav <srvgarg332003@gmail.com>
2024-08-29 16:40:08 +0530

    Update FRONTEND_URL env to CORS_ORIGIN

--- backend/middlewares/cors.go
+++ backend/middlewares/cors.go
@@ -8,7 +8,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", config.GetEnv("FRONTEND_URL"))
+		c.Writer.Header().Set("Access-Control-Allow-Origin", config.GetEnv("CORS_ORIGIN"))
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
